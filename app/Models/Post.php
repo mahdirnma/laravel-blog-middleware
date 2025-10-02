a5de0b3c3d99bdc9eb865b6fed8575c032fc0a52
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-02 20:47:48 +0330

    fill the models and create relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'published_at',
+        'category_id',
+        'tag_id',
+        'is_active',
+    ];
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function tag(){
+        return $this->belongsTo(Tag::class);
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'is_active',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
