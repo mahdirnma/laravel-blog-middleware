61fd3a564df67ef62c9b8ba7cb1ebf1dc8e0832d
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-03 11:52:08 +0330

    use IsAdmin and IsWriter middlewares

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,17 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/',[UserController::class,'index'])->name('home');
-    Route::resource('posts',PostController::class);
-    Route::resource('categories',CategoryController::class);
-    Route::resource('tags',TagController::class);
+    Route::resource('posts',PostController::class)->only('index');
+    Route::resource('categories',CategoryController::class)->only('index');
+    Route::resource('tags',TagController::class)->only('index');
+    Route::middleware('isAdmin')->group(function () {
+        Route::resource('posts',PostController::class)->only('destroy');
+        Route::resource('categories',CategoryController::class)->except('index');
+        Route::resource('tags',TagController::class)->except('index');
+    });
+    Route::middleware('isWriter')->group(function () {
+        Route::resource('posts',PostController::class)->only('create','store','update','edit');
+    });
     Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 });
 Route::middleware('guest')->group(function () {
